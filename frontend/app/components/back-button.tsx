3c1d42fdf17fb4ae4fd2970d4b59574f6ff21fd8
CodeWaveWithAsante <mtech-solutions2000@gmail.com>
2025-05-28 09:36:34 +0530

    First time commit

---
+++ frontend/app/components/back-button.tsx
@@ -0,0 +1,18 @@
+import { ArrowLeft } from "lucide-react";
+import { Button } from "./ui/button";
+import { useNavigate } from "react-router";
+
+export const BackButton = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Button
+      variant="outline"
+      size="sm"
+      onClick={() => navigate(-1)}
+      className="p-4 mr-4"
+    >
+      ← Back
+    </Button>
+  );
+};
