2aacee75abc6108c6472255a97f3cbf414e26144
CodeWaveWithAsante <mtech-solutions2000@gmail.com>
2025-05-29 11:24:58 +0530

    Dashboard

--- backend/routes/workspace.js
+++ backend/routes/workspace.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { validateRequest } from "zod-express-middleware";
-import { workspaceSchema } from "../libs/validate-schema.js";
-import authMiddleware from "../middleware/auth-middleware.js";
 import {
   createWorkspace,
   getWorkspaceDetails,
   getWorkspaceProjects,
   getWorkspaces,
+  getWorkspaceStats,
 } from "../controllers/workspace.js";
+import { workspaceSchema } from "../libs/validate-schema.js";
+import authMiddleware from "../middleware/auth-middleware.js";
 
 const router = express.Router();
 
@@ -22,5 +23,6 @@ router.get("/", authMiddleware, getWorkspaces);
 
 router.get("/:workspaceId", authMiddleware, getWorkspaceDetails);
 router.get("/:workspaceId/projects", authMiddleware, getWorkspaceProjects);
+router.get("/:workspaceId/stats", authMiddleware, getWorkspaceStats);
 
 export default router;
