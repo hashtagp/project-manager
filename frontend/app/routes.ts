af2944f746e5d76572499addacbeec3450281a33
CodeWaveWithAsante <mtech-solutions2000@gmail.com>
2025-06-06 11:48:36 +0530

    Final code

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -33,4 +33,13 @@ export default [
     route("my-tasks", "routes/dashboard/my-tasks.tsx"),
     route("members", "routes/dashboard/members.tsx"),
   ]),
+
+  route(
+    "workspace-invite/:workspaceId",
+    "routes/dashboard/workspaces/workspace-invite.tsx"
+  ),
+
+  layout("routes/user/user-layout.tsx", [
+    route("user/profile", "routes/user/profile.tsx"),
+  ]),
 ] satisfies RouteConfig;
